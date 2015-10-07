f177cae64a10b5c5ebf1ffc9585bac02eb0dd388
Jakob <notrelevant@email.com>
2015-10-07 23:04:15 +0200

    We can now almost display user reminders.

--- app/src/main/java/com/kennethogjakob9000/wakeup/RemindersActivity.java
+++ app/src/main/java/com/kennethogjakob9000/wakeup/RemindersActivity.java
@@ -1,15 +1,24 @@
 package com.kennethogjakob9000.wakeup;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RemindersActivity extends AppCompatActivity {
@@ -17,14 +26,15 @@ public class RemindersActivity extends AppCompatActivity {
     ArrayList<String> remindUsers = null;
     Set<String> users = null;
     private String username = "";
-
+    public class userData  {
+        String name = "";
+    }
     @Override
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
 
-        remindUsers = getIntent().getStringArrayListExtra(StartMap.REMIND);
-        username = getIntent().getStringExtra(LoginScreen.USERNAME);
+
     }
 
     protected void onResume() {
@@ -36,6 +46,23 @@ public class RemindersActivity extends AppCompatActivity {
         }
 
         username = getIntent().getStringExtra(LoginScreen.USERNAME);
+
+        remindUsers = getIntent().getStringArrayListExtra(StartMap.REMIND);
+        username = getIntent().getStringExtra(LoginScreen.USERNAME);
+
+        final Adapter adapter = new Adapter();
+        ListView listview = (ListView)findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                userData data = adapter.getUserdata(position);
+                Toast.makeText(RemindersActivity.this,data.name,Toast.LENGTH_LONG).show();
+            }
+        }
+        );
+
     }
 
     @Override
@@ -85,4 +112,51 @@ public class RemindersActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    public class Adapter extends BaseAdapter {
+
+        List<userData> data = getDataForListView();
+
+        @Override
+        public int getCount () {
+            return data.size();
+        }
+
+        @Override
+        public userData getItem (int position) {
+            return data.get(position);
+        }
+
+        @Override
+        public long getItemId (int position) {
+            return position;
+        }
+
+        @Override
+        public View getView (int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.listitem,parent,false);
+            }
+            TextView text = (TextView)convertView.findViewById(R.id.name_of_user);
+            userData userdata = data.get(position);
+            text.setText(userdata.name);
+            return convertView;
+        }
+
+        public userData getUserdata (int position) {
+            return data.get(position);
+        }
+    }
+
+    List<userData> getDataForListView() {
+        List<userData> list = new ArrayList<userData>();
+        for (String s: remindUsers) {
+            userData userdata = new userData();
+            userdata.name = s;
+            list.add(userdata);
+        }
+        return list;
+    }
+
 }
